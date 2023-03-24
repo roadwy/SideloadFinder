808b9841a81204a5462ec36e05136c62a42444d0
dd <ddd@gmail.com>
2023-03-24 23:20:47 +0800

    feat: support print exploting dll function name

--- sideload_finder.py
+++ sideload_finder.py
@@ -9,40 +9,20 @@ import psutil
 import os
 import argparse
 
-exe_name = ""
-out_name = ""
 
-
-def on_message(message, data):
-    flag = message["payload"]["Flag"]
-    LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020
-    if flag & LOAD_LIBRARY_AS_IMAGE_RESOURCE:
-        return
-    msg = "{},{},0x{:x}\n".format(exe_name, message["payload"]["DllName"], message["payload"]["Flag"])
-    print(msg)
-    if len(out_name) == 0:
-        return
-    with open(out_name, "a+") as f:
-        f.write(msg)
-
-
-def create_process(exe_path):
-    global exe_name 
-    exe_name = exe_path
-    try:
-        process = subprocess.Popen(exe_path, creationflags=CREATE_SUSPENDED)
-        return process.pid
-    except Exception as e:
-        return None
-
-
-def frida_hook(pid, timeout=3):
-    session = frida.attach(pid)
-    script = session.create_script("""
+class SideLoadFinder:
+    def __init__(self, out_path, timeout):
+        self.out_path = out_path
+        self.timeout = timeout
+        self.LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020
+        self.image_path = ""
+        self.line_content = ""
+        self.frida_script = """
 var LoadLibraryEx = Module.findExportByName("api-ms-win-core-libraryloader-l1-2-0.dll", "LoadLibraryExW");
 if (LoadLibraryEx == 0) {
     LoadLibraryEx = Module.findExportByName("kernelbase.dll", "LoadLibraryExW");
 }
+var FakeModule = 0x777777;
 Interceptor.attach(LoadLibraryEx, {
     onEnter: function(args, state) {
         this.lpLibFileName = Memory.readUtf16String(args[0]);
@@ -51,40 +31,82 @@ Interceptor.attach(LoadLibraryEx, {
     onLeave: function(retval, state) {
         if (retval == 0) {
             if (this.dwFlags != 2) {
-                send({"DllName":this.lpLibFileName, "Flag":this.dwFlags})
+                send({"payload_type":"dll", "dll":this.lpLibFileName, "flag":this.dwFlags});
+                retval.replace(FakeModule);
             }
         }
     }
 });
-    """)
-    script.on('message', on_message)
-    script.load()
-    p = psutil.Process(pid)
-    p.resume()
-    try:
-        p.wait(timeout=timeout)
-    except Exception as e:
-        p.kill()
-    session.detach()
+var GetProcAddress = Module.findExportByName("kernel32.dll", "GetProcAddress");
+Interceptor.attach(GetProcAddress, {
+    onEnter: function(args, state) {
+        this.Procname = Memory.readAnsiString(args[1]);
+        this.module = args[0];
+        if(this.module == FakeModule){
+        send({"payload_type":"proc", "proc":this.Procname});
+        }
+    },
+});
+"""
 
+    def on_message(self, message, data):
+        if message["type"] == "send":
+            print(message)
+            if message["payload"]["payload_type"] == "dll":
+                self.handle_dll_msg(message)
+            else:
+                self.handle_proc_msg(message)
+        else:
+            print(message)
 
-def get_sample_files(dir_path):
-    exe_file = set()
-    for parent, _, filenames in os.walk(dir_path):
-        for filename in filenames:
-            if filename.lower().endswith(".exe") == False:
-                continue
-            file_path = os.path.join(parent, filename)
-            exe_file.add(file_path)
-    return exe_file
+    def handle_dll_msg(self, message):
+        self.out_csv()
+        flag = message["payload"]["flag"]
+        if flag & self.LOAD_LIBRARY_AS_IMAGE_RESOURCE:
+            return
+        self.line_content = "{},{},0x{:x}".format(self.image_path, message["payload"]["dll"], flag)
 
+    def handle_proc_msg(self, message):
+        proc = message["payload"]["proc"]
+        self.line_content = "{},{}".format(self.line_content, proc)
 
-def find_sideload(samples_dir):
-    exes = get_sample_files(samples_dir)
-    for exe in exes:
-        pid = create_process(exe)
-        if pid is not None:
-            frida_hook(pid)
+    def out_csv(self):
+        if self.line_content == "":
+            return
+        self.line_content = self.line_content + "\n"
+        with open(self.out_path, "a+") as f:
+            f.write(self.line_content)
+            print(self.line_content)
+            self.line_content = ""
+
+    def finder(self, image_path):
+        try:
+            self.image_path = image_path
+            pid = subprocess.Popen(self.image_path, creationflags=CREATE_SUSPENDED).pid
+            session = frida.attach(pid)
+            script = session.create_script(self.frida_script)
+            script.on('message', self.on_message)
+            script.load()
+            p = psutil.Process(pid)
+            p.resume()
+            try:
+                p.wait(timeout=self.timeout)
+            except Exception as e:
+                print(e)
+                p.kill()
+            session.detach()
+        except Exception as e:
+            print(e)
+        finally:
+            self.out_csv()
+
+    def run(self, image_dir):
+        for parent, _, filenames in os.walk(image_dir):
+            for filename in filenames:
+                if not filename.lower().endswith(".exe"):
+                    continue
+                image_path = os.path.join(parent, filename)
+                self.finder(image_path)
 
 
 if __name__ == '__main__':
@@ -95,6 +117,8 @@ if __name__ == '__main__':
     if not args.i or not args.o:
         parser.print_help()
         exit(1)
-    out_name = args.o
-    find_sideload(args.i)
+    out_path = args.o
+    image_dir = args.i
+    finder = SideLoadFinder(out_path, 5)
+    finder.run(image_dir)
 
